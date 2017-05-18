52e856b10c058b7b03bfa26527557f21a4b4d51d
Sebastián Salata Ruiz-Tagle <sa.salatart@gmail.com>
2017-05-18 00:30:22 -0400

    add new env variable for full mongo uri

--- server/config/db.js
+++ server/config/db.js
@@ -2,5 +2,5 @@ const config = require('.');
 const mongoose = require('mongoose');
 const bluebird = require('bluebird');
 
-mongoose.connect(`mongodb://${config.DB_HOST}:${config.DB_PORT}/acm-kiosk`);
+mongoose.connect(config.MONGODB_URI || `mongodb://${config.DB_HOST}:${config.DB_PORT}/acm-kiosk`);
 mongoose.Promise = bluebird;
--- server/config/index.js
+++ server/config/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   HOST: process.env.HOST || 'localhost',
   PORT: process.env.PORT || '8888',
+  MONGODB_URI: process.env.MONGODB_URI,
   DB_HOST: process.env.MONGO_HOST || 'mongo_db',
   DB_PORT: process.env.MONGO_PORT || '27017',
   SECRET_TOKEN: process.env.SECRET_TOKEN || 'napoleon',
